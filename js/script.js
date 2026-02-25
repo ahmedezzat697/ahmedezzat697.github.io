f9b81030389775bb35f893ab4671f43f24d75810
Ahmed Ezzat <118674296+ahmedezzat697@users.noreply.github.com>
2026-02-26 01:24:46 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const navitem = document.getElementsByClassName("nav-link");
+const btnChek = document.getElementById("btn-check");
+const btnCv = document.getElementById("cv");
+const menuIcon = document.getElementById("'menu-icon'");
+const navbar = document.querySelector('.item');
+const navbg = document.querySelector('.nav-bg');
+
+
+/////////////////////////////////////Hedear////////////////////////////////////////
+
+
+/////////////////////////////////////Home////////////////////////////////////////
+
+btnCv.addEventListener('click', function () {
+    alert("Not available now")
+})
+
+/////////////////////////////////certificates/////////////////////////////////////
+btnChek.addEventListener("click", function() {
+    window.open("../img/certificate-1.jpg", "_blank");
+});                 
